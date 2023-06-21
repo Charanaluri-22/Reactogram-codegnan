91c8ca13e43de487202aab8075deaddcb89275be
charan <aluricharan1922@gmail.com>
2023-06-21 12:25:48 +0530

    modified config.js

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,4 @@
-// module.exports = {
-//   MONGOBD_URL:   "mongodb+srv://aluricharan1922:aluricharan@cluster0.eeclpk7.mongodb.net/project?retryWrites=true&w=majority",
-//   JWT_SECRET: "CharanAluri192217296174mru",
-// };
+
 const dotenv = require('dotenv');
 dotenv.config(); // Load the environment variables from .env file
 
